9a9001dad493f33279294c8ad3c1c64d6ac05a98
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-02 07:57:48 +0100

    Simplify Event.ts

--- handler/Event.ts
+++ handler/Event.ts
@@ -2,12 +2,9 @@ import fs from "fs";
 import Eris from "eris";
 
 export default async (client: Eris.Client) => {
-  const events = fs.readdirSync("./events/"), custom_events = fs.readdirSync("./custom_events/");
-  for (const event of events) {
-    client.on(event.split(".")[0], (...args) => require(`../events/${event}`)(client, ...args));
-  };
+  const events = fs.readdirSync("./events/"), custom_events = fs.readdirSync("./moderationRegistry/");
+
+  for (const evt of events) client.on(evt.split(".")[0], (...args) => require(`../events/${evt}`)(client, ...args));
   
-  for (const event of custom_events) {
-    client.on(event.split(".")[0], (...args) => require(`../custom_events/${event}`)(client, ...args));
-  };
+  for (const evt of custom_events) client.on(evt.split(".")[0], (...args) => require(`../moderationRegistry/${evt}`)(client, ...args));
 };
