7506d1d2059a53f632fc4f9fb8ca0d9da79a2ae0
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-02 09:08:07 +0100

    Fixed sudden offline.

--- events/messageUpdate.ts
+++ events/messageUpdate.ts
@@ -3,7 +3,7 @@ import config from "../config";
 
 export = async (client: Eris.Client, message: Eris.Message, oldMessage: Eris.OldMessage) => {
   // ignore
-  if (message.author === client.user || message.author.bot) return;
+  if (message.author === client.user || message.author?.bot) return;
 
   // one word story
   if (config.channel.onewordstory.includes(message.channel.id)) {
