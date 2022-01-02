b7a1594b5c808f8d64b8df647aff6a6c857f8d51
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-02 07:57:24 +0100

    Modified welcome/goodbye messages.

--- events/guildMemberAdd.ts
+++ events/guildMemberAdd.ts
@@ -1,15 +1,22 @@
 import Eris from "eris";
 import config from "../config";
 import {stripIndents} from "common-tags";
+import db from "quick.db";
 
 export = async (client: Eris.Client, guild: Eris.Guild, member: Eris.Member) => {
   if (guild.id !== config.guildID || member.bot) return;
 
+  let returnMember: FarewellMemberConclusion = await db.get(`replaceWelcomeMessageUser.${member.user.id}`);
+
+  let returnMemberMessage = returnMember ? 
+  `Selamat datang kembali di Discord server, **${member.guild.name}**!` :
+  stripIndents`
+  Selamat datang di Discord server, **${member.guild.name}**!
+  Semoga betah, dan jangan lupa baca ${client.getChannel("274351350656139265").mention || "<#274351350656139265>"} sebelum ngobrol!`
+
   // Embed
   let embeds = new Eris.RichEmbed().setColor(0x24C86E).setTimestamp()
   .setTitle(`Halo, ${member.user.username}#${member.user.discriminator} 👋`)
-  .setDescription(stripIndents`
-  Selamat datang di Discord server, **${member.guild.name}**!
-  Semoga betah, dan jangan lupa baca ${client.getChannel("274351350656139265").mention || "<#274351350656139265>"} sebelum ngobrol!`);
+  .setDescription(returnMemberMessage);
   return client.createMessage(config.channel.general, {content: member.mention, embeds: [embeds]});
 };
--- events/guildMemberRemove.ts
+++ events/guildMemberRemove.ts
@@ -1,15 +1,26 @@
 import Eris from "eris";
 import config from "../config";
-import {stripIndents} from "common-tags";
+import db from "quick.db";
+import ms from "ms";
 
-export = async (client, guild, member) => {
+export = async (client, guild: Eris.Guild, member: Eris.Member) => {
   if (guild.id !== config.guildID || member.bot) return;
 
-  // db.set(`replaceWelcomeMessageUser.${member.user.id}`, true);
+  db.set(`replaceWelcomeMessageUser.${member.user.id}`, {
+    activateWhenComingBack: true,
+    leavingSince: Date.now()
+  });
 
   // Embed
   let embeds = new Eris.RichEmbed().setColor(0xC82427).setTimestamp()
-  .setTitle(`Farewell.`).setDescription(`**${member.user.username}** telah keluar dari server.`);
+
+  if (Math.floor(Date.now() - member.joinedAt!) < ms("1m")) {
+    embeds.setTitle(`Dadah...`).setDescription(`Yah, **${member.user.username}#${member.user.discriminator}** langsung keluar...`);
+  }
+
+  else {
+    embeds.setTitle(`Farewell.`).setDescription(`**${member.user.username}#${member.user.discriminator}** telah keluar dari server. Dadah~`);
+  }
   
   return client.createMessage(config.channel.general, {embeds: [embeds]});
 };
