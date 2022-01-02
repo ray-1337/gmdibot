c84a68aec9ddd8462c2f4dec4ffc5608143be85d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-02 09:10:26 +0100

    replaced export default

--- handler/Event.ts
+++ handler/Event.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import Eris from "eris";
 
-export default async (client: Eris.Client) => {
+export = async (client: Eris.Client) => {
   const events = fs.readdirSync("./events/"), custom_events = fs.readdirSync("./moderationRegistry/");
 
   for (const evt of events) client.on(evt.split(".")[0], (...args) => require(`../events/${evt}`)(client, ...args));
