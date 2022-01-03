b4991fe4aec7f93aab545f78fcf1d3af10505b64
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-03 11:19:08 +0100

    interval improvement

---
+++ factory/CRON.ts
@@ -0,0 +1,81 @@
+import { scheduleJob } from "node-schedule";
+import Eris from "eris";
+import db from "quick.db";
+import Config from "../config";
+import ms from "ms";
+
+export default async function (client: Eris.Client) {
+  scheduleJob('* 1 * * *', async function() {
+    let warnings: [string, WarningLastedOptions][] = Object.entries(db.get("warningLasted"));
+
+    for (const [id, val] of warnings) {
+      if (val.due) {
+        let userID = id || val.memberID;
+        switch (val.level) {
+          case 1: {
+            client.removeGuildMemberRole(Config.guildID, userID, Config.warning.role[1]);
+            break;
+          };
+
+          case 2: {
+            let memberREST = client.guilds.get(Config.guildID)?.members.get(Config.guildID) || await client.getRESTGuildMember(Config.guildID, userID);
+            if (memberREST) {
+              // are you sure that we'll have to use the same amount of timeout again for Warn I?
+              client.editGuildMember(Config.guildID, userID, {
+                roles: [...memberREST.roles.filter((value) => {
+                  let combinedWarningRoles = [Config.warning.role[1], Config.warning.role[2], Config.warning.role[3]];
+                  if (!combinedWarningRoles.includes(value)) return true;
+                }), Config.warning.role[1]]
+              })
+              .catch(err => console.error(err));
+            };
+            break;
+          };
+
+          case 3: {
+            let memberREST = client.guilds.get(Config.guildID)?.members.get(Config.guildID) || await client.getRESTGuildMember(Config.guildID, userID);
+            if (memberREST) {
+              // are you sure that we'll have to use the same amount of timeout again for Warn I?
+              client.editGuildMember(Config.guildID, userID, {
+                roles: val.previousRoleList?.filter((value) => {
+                  let combinedWarningRoles = [Config.warning.role[1], Config.warning.role[2], Config.warning.role[3]];
+                  if (!combinedWarningRoles.includes(value)) return true;
+                })
+                .concat(Config.warning.role[2])
+              })
+              .catch(err => console.error(err));
+            };
+            break;
+          };
+
+          default:
+            break;
+        };
+
+        // reset the timeout
+        Promise.all([
+          db.set(`warningLasted.${userID}.due`, null),
+          db.set(`warningLasted.${userID}.since`, null),
+          db.set(`warningLasted.${userID}.full`, null)
+        ]);
+
+        continue;
+      };
+    };
+  });
+
+  scheduleJob('* 12 * * *', async function() {
+    let memberLeaveProposal: [string, FarewellMemberConclusion][] = Object.entries(db.get("replaceWelcomeMessageUser"));
+    for (const [id, val] of memberLeaveProposal) {
+      if (Math.floor(Date.now() - val.leavingSince) > ms("14d")) {
+        try {
+          db.delete(`replaceWelcomeMessageUser.${id || val.memberID}`);
+        } catch (err) {
+          console.error(err);
+        };
+
+        continue;
+      };
+    };
+  });
+};
