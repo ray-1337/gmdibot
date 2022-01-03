796f8c353bffea6dd2b903fc51e3215036619ef1
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-03 11:18:46 +0100

    feature improvement

--- interactionRegistry/Warn.ts
+++ interactionRegistry/Warn.ts
@@ -3,10 +3,14 @@ import Config from "../config";
 import Util from "../handler/Util";
 import db from "quick.db";
 import ms from "ms";
-const util = new Util();
+import parseDur from "parse-duration";
+import prettyMS from "pretty-ms";
+import centra from "centra";
 
 export = async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
   await interaction.defer();
+
+  const util = new Util();
 
   // some interaction bug
   if (!interaction || !interaction.data || !interaction.data.options) {
@@ -31,40 +35,100 @@ export = async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
         return interaction.createMessage("Some content were missing. Please try again.");
       };
 
+      // console.log(optionsBehind);
+
       let member = (optionsBehind as Eris.InteractionDataOptionsUser[]).filter(x => x.name === "member" && x.type === ErisTypes.USER)[0];
-      let level = (optionsBehind as Eris.InteractionDataOptionsInteger[]).filter(x => x.name === "level" && x.type === ErisTypes.INTEGER)[0];
-      let reason = (optionsBehind as Eris.InteractionDataOptionsString[]).filter(x => x.name === "reason" && x.type === ErisTypes.STRING)[0];
-      
-      if (!reason || !reason.value) reason.value = "Not provided.";
 
       let memberValidation = client.guilds.get(Config.guildID)?.members.get(member.value);
       if (!memberValidation) {
         return interaction.createMessage("Member is not in guild, nor cached. Sorry.");
       };
 
+      if (memberValidation.bot === true || memberValidation.user.bot === true) {
+        return interaction.createMessage("You can't warn a bot.");
+      };
+
+      let level = (optionsBehind as Eris.InteractionDataOptionsInteger[]).filter(x => x.name === "level" && x.type === ErisTypes.INTEGER)[0];
+      let reason = (optionsBehind as Eris.InteractionDataOptionsString[]).filter(x => x.name === "reason" && x.type === ErisTypes.STRING)[0];
+      
+      // optional params, considerate
+      let timeout = (optionsBehind as Eris.InteractionDataOptionsString[]).filter(x => x.name === "timeout" && x.type === ErisTypes.STRING)[0];
+      let evidence = (optionsBehind as Eris.InteractionDataOptionsString[]).filter(x => x.name === "evidence" && x.type === ErisTypes.STRING)[0];
+
+      if (!reason || !reason.value) reason.value = "Not provided.";
+
       let warningLevelExplained;
       switch (level.value) {
-        case 1: warningLevelExplained = "I"; break;
-        case 2: warningLevelExplained = "II"; break;
-        case 3: warningLevelExplained = "III"; break;
+        case 1: warningLevelExplained = "I / 01"; break;
+        case 2: warningLevelExplained = "II / 02"; break;
+        case 3: warningLevelExplained = "III / 03"; break;
         default: warningLevelExplained = "Unknown."; break;
       };
 
       embed
       .setAuthor(`${memberValidation.username}#${memberValidation.discriminator}`, undefined, memberValidation.user.dynamicAvatarURL("png", 128))
-      .setColor(0x121112)
-      .addField("User ID", memberValidation.id, true)
+      .setColor(0x464646)
+      .setFooter(`ID: ${memberValidation.id}`)
+      // .addField("User ID", memberValidation.id, true)
       .addField("Moderator", `${interaction.member?.mention}`, true) // client.users.get(interaction.data.resolved?.users?.keys().next().value)?.mention
       .addField("Warning Level", warningLevelExplained, true)
       .addField("Reason", reason.value, true);
+
+      let fileContent: Eris.FileContent | undefined = undefined;
+      if (evidence) {
+        // no need regex shit lmao, i love it.
+        try {
+          new URL(evidence.value);
+        } catch (error) {
+          return interaction.createMessage("Failed to parse the URL.");
+        };
+
+        let request = await centra(evidence.value, "GET").timeout(10000).send();
+        if (!request || (request.statusCode && request.statusCode >= 400)) {
+          return interaction.createMessage("Failed to load evidence content.");
+        };
+
+        if (!request.headers["content-type"]?.match(/^(image\/\w+)/gi)) {
+          return interaction.createMessage("Invalid evidence type.");
+        };
+
+        let ext = util.contentTypeDecide(request.headers["content-type"]);
+        if (!ext) {
+          return interaction.createMessage("Unable to find extension for the evidence.");
+        };
+
+        // link could be removed. use buffer instead.
+        embed.setImage(`attachment://warningPart.userID_${memberValidation.id}.${ext}`);
+
+        fileContent = {
+          name: `warningPart.userID_${memberValidation.id}.${ext}`,
+          file: Buffer.from(request.body)
+        };
+      };
 
       let combinedWarningRoles = [Config.warning.role[1], Config.warning.role[2], Config.warning.role[3]];
       if (memberValidation.roles.some(x => x === Config.warning.role[level.value])) {
         return interaction.createMessage("Already executed. Try either select a different level or remove the warning role.");
       };
 
+      let formatTime: Intl.DateTimeFormatOptions = {
+        hour12: false,
+        timeZone: "Asia/Jakarta",
+        weekday: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        month: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      };
+
       // Warn III
       if (level.value === 3) {
+        if (timeout) {
+          return interaction.createMessage(`This warning level cannot be presented with timeout. Warn III timeout must be randomized from 30 to 90 days.`);
+        };
+
         client.editGuildMember(Config.guildID, member.value, {roles: [Config.warning.role[level.value]]})
         .catch((error) => {
           console.error(error);
@@ -78,9 +142,10 @@ export = async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
         db.set(`warningLasted.${member.value}`, {
           since: Date.now(),
           due: conversionToDay,
-          full: new Date(Date.now() + conversionToDay),
+          full: new Date(Date.now() + conversionToDay).getTime(),
           memberID: member.value,
-          previousRoleList: memberValidation.roles
+          previousRoleList: memberValidation.roles,
+          level: level.value
         });
 
         // debugging
@@ -88,17 +153,7 @@ export = async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
         
         embed
         .addField("Length", `${due} days`, true)
-        .addField("Expiration", new Date(Date.now() + conversionToDay).toLocaleString('id-ID', {
-          hour12: false,
-          timeZone: "Asia/Jakarta",
-          weekday: 'short',
-          day: 'numeric',
-          year: 'numeric',
-          month: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-          second: 'numeric',
-        }) + " WIB");
+        .addField("Expiration", new Date(Date.now() + conversionToDay).toLocaleString('id-ID', formatTime) + " WIB");
       }
 
       // Warn I/II
@@ -108,6 +163,41 @@ export = async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
         //   console.error(error);
         //   return interaction.createMessage(`**Encountered error when assigning someone's role:** ${error.code || 0} / ${error.message}`);
         // });
+        if (timeout) {
+          // custom
+          parseDur["nanosekon"] = parseDur["ns"];
+          parseDur["mikrosekon"] = parseDur["μs"];
+          parseDur["milisekon"] = parseDur["ms"];
+          parseDur["sekon"] = parseDur["s"];
+          parseDur["menit"] = parseDur["m"];
+          parseDur["jam"] = parseDur["h"];
+          parseDur["hari"] = parseDur["d"];
+          parseDur["minggu"] = parseDur["wk"];
+          parseDur["bulan"] = parseDur["months"];
+          parseDur["tahun"] = parseDur["yr"];
+
+          let parsedTime = parseDur(timeout.value);
+          if (!parsedTime) {
+            return interaction.createMessage("Unable to parse timeout format.");
+          };
+
+          if (parsedTime < ms("1h") || parsedTime > ms("365d")) {
+            return interaction.createMessage("The timeout parameter is out of limit. Should be more than an hour, less than a year or equivalent.");
+          };
+  
+          db.set(`warningLasted.${member.value}`, {
+            since: Date.now(),
+            due: parsedTime,
+            full: new Date(Date.now() + parsedTime).getTime(),
+            memberID: member.value,
+            previousRoleList: memberValidation.roles,
+            level: level.value
+          });
+
+          embed
+          .addField("Length", prettyMS(parsedTime, {verbose: !0}), true)
+          .addField("Expiration", new Date(Date.now() + parsedTime).toLocaleString('id-ID', formatTime) + " WIB");
+        };
 
         // why. god. (temporary solution, cus im stinky, havent take a shower for 69 days.)
         let memberValidateRolesNewArr: string[] = [];
@@ -125,15 +215,20 @@ export = async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
         });
       }
 
-      else return;
+      else {
+        return interaction.createMessage(`Unable to parse warning levels.`);
+      };
 
-      client.createMessage(Config.warning.channel.warning, {embeds: [embed]})
+      client.createMessage(Config.warning.channel.warning, {embeds: [embed]}, fileContent)
+      .then(x => {
+        if (timeout && level.value < 3) db.set(`warningLasted.${member.value}.warningLogID`, x.id);
+      })
       .catch((error) => {
         console.error(error);
         return interaction.createMessage(`**Encountered error when posting a warning log:** \n${error.code || 0} / ${error.message}`);
       });
 
-      return interaction.createMessage(`Set warning to ${memberValidation.mention} with level **${level.value}** succeeded.`);
+      return interaction.createMessage(`Successfully set warning to **${memberValidation.username}#${memberValidation.discriminator}** with level **${warningLevelExplained}**.`);
 
     default:
       break;
