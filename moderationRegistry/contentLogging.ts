5818ebae9c5d06050decd2d1cf1145b71cef1b4c
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-03 11:42:21 +0100

    make sure it is a buffer

--- moderationRegistry/contentLogging.ts
+++ moderationRegistry/contentLogging.ts
@@ -35,7 +35,7 @@ export = async (client: Eris.Client, message: Eris.Message) => {
       if (!videoRegexMimeType.test(message.attachments[0].content_type!)) embed.setImage(message.attachments[0].proxy_url);
 
       centra(message.attachments[0].proxy_url, "GET").send()
-      .then(res => fs.writeFileSync(`/home/ray/gmdi-server/content/${generatedFileName}`, res.body))
+      .then(res => fs.writeFileSync(`/home/ray/gmdi-server/content/${generatedFileName}`, Buffer.from(res.body)))
       .catch(console.error);
     }
 
@@ -47,7 +47,7 @@ export = async (client: Eris.Client, message: Eris.Message) => {
         let generatedFileName = `${message.author.id}.${util.generateHash(defaultHashLength)}.${extension}`;
 
         centra(content.proxy_url, "GET").send()
-        .then(res => fs.writeFileSync(`/home/ray/gmdi-server/content/${generatedFileName}`, res.body))
+        .then(res => fs.writeFileSync(`/home/ray/gmdi-server/content/${generatedFileName}`, Buffer.from(res.body)))
         .catch(console.error);
 
         listDeletedContent.push(mainEndpoint + generatedFileName);
