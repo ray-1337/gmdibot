10f216b286742149ef06b9580adb4af7c154f9dd
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-09 08:51:16 +0100

    a bit passive leave message

--- events/guildMemberRemove.ts
+++ events/guildMemberRemove.ts
@@ -3,7 +3,7 @@ import config from "../config";
 import db from "quick.db";
 import ms from "ms";
 
-export = async (client, guild: Eris.Guild, member: Eris.Member) => {
+export = async (client: Eris.Client, guild: Eris.Guild, member: Eris.Member) => {
   if (guild.id !== config.guildID || member.bot) return;
 
   db.set(`replaceWelcomeMessageUser.${member.user.id}`, {
@@ -15,12 +15,12 @@ export = async (client, guild: Eris.Guild, member: Eris.Member) => {
   // Embed
   let embeds = new Eris.RichEmbed().setColor(0xC82427).setTimestamp();
 
-  if (Math.floor(Date.now() - member.joinedAt!) < ms("1m")) {
-    embeds.setTitle(`Dadah...`).setDescription(`Yah, **${member.user.username}#${member.user.discriminator}** langsung keluar...`);
+  if (Math.floor(Date.now() - member.joinedAt!) < ms("5m")) {
+    embeds.setTitle(`Dadah...`).setDescription(`**${member.user.username}#${member.user.discriminator}** langsung keluar dari server.`);
   }
 
   else {
-    embeds.setTitle(`Farewell.`).setDescription(`**${member.user.username}#${member.user.discriminator}** keluar dari server. Take care ;)`);
+    embeds.setTitle(`Farewell.`).setDescription(`**${member.user.username}#${member.user.discriminator}** keluar dari server.`);
   };
   
   return client.createMessage(config.channel.general, {embeds: [embeds]});
