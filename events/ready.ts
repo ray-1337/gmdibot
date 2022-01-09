f9d16f5f605d6e7e22bfcbb43f723ad869a32c9e
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-09 12:53:56 +0100

    edit bot status

--- events/ready.ts
+++ events/ready.ts
@@ -1,8 +1,9 @@
-import Eris from "eris";
+import GMDIBot from "../handler/Client";
 import CRON from "../factory/CRON";
 
-export = async (client: Eris.Client) => {
-  client.editStatus("idle", {type: 3, name: "Geometry Dash Indonesia"});
+export = async (client: GMDIBot) => {
+  client.editStatus("idle", {type: 3, name: "Anime"});
+
   console.log("Ready.");
 
   client.emit("removalChannelCooldown", client);
