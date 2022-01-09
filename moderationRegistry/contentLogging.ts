f7e03d3201babfe1ebf14c5df503087cdfbc6d06
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-09 13:27:19 +0100

    add try catch to prevent offline

--- moderationRegistry/contentLogging.ts
+++ moderationRegistry/contentLogging.ts
@@ -1,80 +1,83 @@
 import Eris from "eris";
 import Util from "../handler/Util";
 import centra from "centra";
-import {stripIndents} from "common-tags";
+import { stripIndents } from "common-tags";
 import config from "../config";
 import fs from "fs";
 
 const util = new Util();
 
 export = async (client: Eris.Client, message: Eris.Message) => {
-  // console.log(message);
-  if (!message || message.member?.user.bot) return;
+  try {
+    if (!message || message.member?.user.bot) return;
 
-  const embed = new Eris.RichEmbed().setColor(0x242424).setTitle("Deleted Content")
-  .setAuthor(`${message.author.username}#${message.author.discriminator}`, undefined, message.author.dynamicAvatarURL("png", 128))
-  .addField("User Information", stripIndents`
+    const embed = new Eris.RichEmbed().setColor(0x242424).setTitle("Deleted Content")
+      .setAuthor(`${message.member?.user.username}#${message.member?.user.discriminator}`, undefined, message.member?.user.dynamicAvatarURL("png", 128))
+      .addField("User Information", stripIndents`
   **Channel:** ${message.channel.mention}
-  **Caption:** ${util.truncate(message.content, 64) || "[Tidak Ada]"}
-  **User ID:** ${message.author.id}`);
+  ${message.content?.length > 0 ? `**Caption:** ${util.truncate(message.content, 64)}` : ""}
+  **User ID:** ${message.member?.user.id}`);
 
-  let defaultHashLength: number = 9;
-  let videoRegexMimeType: RegExp = /^(video)\/.*/gi;
-  let listDeletedContent: string[] = [];
-  let mainEndpoint = config.endpoint.contentLogging;
+    let defaultHashLength: number = 9;
+    let videoRegexMimeType: RegExp = /^(video)\/.*/gi;
+    let listDeletedContent: string[] = [];
+    let mainEndpoint = config.endpoint.contentLogging;
 
-  if (message.attachments && message.attachments.length >= 1) {
-    if (message.attachments.length === 1) {
-      let extension = util.contentTypeDecide(message.attachments[0].content_type!);
-      if (extension == null) return;
+    if (message.attachments && message.attachments.length >= 1) {
+      if (message.attachments.length === 1) {
+        let extension = util.contentTypeDecide(message.attachments[0].content_type!);
+        if (extension == null) return;
 
-      let generatedFileName = `${message.author.id}.${util.generateHash(defaultHashLength)}.${extension}`;
+        let generatedFileName = `${message.member?.user.id}.${util.generateHash(defaultHashLength)}.${extension}`;
 
-      embed.addField("Backup Endpoint", mainEndpoint + generatedFileName);
-      
-      if (!videoRegexMimeType.test(message.attachments[0].content_type!)) embed.setImage(message.attachments[0].proxy_url);
+        embed.addField("Backup Endpoint", mainEndpoint + generatedFileName);
 
-      centra(message.attachments[0].proxy_url, "GET").send()
-      .then(res => {
-        let stream = fs.createWriteStream(`/home/ray/gmdi-server/content/${generatedFileName}`);
-        stream.once('open', () => {
-          stream.write(Buffer.from(res.body));
-          stream.end();
-        });
-      })
-      .catch(console.error);
+        if (!videoRegexMimeType.test(message.attachments[0].content_type!)) embed.setImage(message.attachments[0].proxy_url);
+
+        centra(message.attachments[0].proxy_url, "GET").send()
+          .then(res => {
+            let stream = fs.createWriteStream(`/home/ray/gmdi-server/content/${generatedFileName}`);
+            stream.once('open', () => {
+              stream.write(Buffer.from(res.body));
+              stream.end();
+            });
+          })
+          .catch(console.error);
+      }
+
+      else if (message.attachments.length > 1) {
+        for (let content of message.attachments) {
+          let extension = util.contentTypeDecide(message.attachments[0].content_type!);
+          if (extension == null) continue;
+
+          let generatedFileName = `${message.member?.user.id}.${util.generateHash(defaultHashLength)}.${extension}`;
+
+          centra(content.proxy_url, "GET").send()
+            .then(res => {
+              // fs.writeFileSync(`/home/ray/gmdi-server/content/${generatedFileName}`, Buffer.from(res.body))
+              let stream = fs.createWriteStream(`/home/ray/gmdi-server/content/${generatedFileName}`);
+              stream.once('open', () => {
+                stream.write(Buffer.from(res.body));
+                stream.end();
+              });
+            })
+            .catch(console.error);
+
+          listDeletedContent.push(mainEndpoint + generatedFileName);
+        };
+
+        embed.addField(`Backup Endpoint (Total: ${listDeletedContent.length})`, listDeletedContent.map(x => `- ${x}`).join("\n"))
+      };
     }
 
-    else if (message.attachments.length > 1) {
-      for (let content of message.attachments) {
-        let extension = util.contentTypeDecide(message.attachments[0].content_type!);
-        if (extension == null) continue;
+    else return;
 
-        let generatedFileName = `${message.author.id}.${util.generateHash(defaultHashLength)}.${extension}`;
+    // work in progress.
+    // if (message.embeds) {
+    // };
 
-        centra(content.proxy_url, "GET").send()
-        .then(res => {
-          // fs.writeFileSync(`/home/ray/gmdi-server/content/${generatedFileName}`, Buffer.from(res.body))
-          let stream = fs.createWriteStream(`/home/ray/gmdi-server/content/${generatedFileName}`);
-          stream.once('open', () => {
-            stream.write(Buffer.from(res.body));
-            stream.end();
-          });
-        })
-        .catch(console.error);
-
-        listDeletedContent.push(mainEndpoint + generatedFileName);
-      };
-
-      embed.addField(`Backup Endpoint (Total: ${listDeletedContent.length})`, listDeletedContent.map(x => `- ${x}`).join("\n"))
-    };
-  }
-
-  else return;
-
-  // work in progress.
-  // if (message.embeds) {
-  // };
-
-  return client.createMessage(config.channel.modlog, {embeds: [embed]});
+    return client.createMessage(config.channel.modlog, { embeds: [embed] });
+  } catch (error) {
+    console.error(error);
+  };
 };
