f86da908f7c27a0d86ed4c77f7b55bccca35033f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-19 03:32:19 +0100

    added point command

---
+++ interactionRegistry/Point.ts
@@ -0,0 +1,14 @@
+import Eris from "eris";
+import db from "quick.db";
+import Config from "../config";
+
+export default async (client: Eris.Client, interaction: Eris.CommandInteraction) => {
+  await interaction.defer();
+
+  let userChatDB: UserChatInterface = db.get(`userChatRate.${interaction.user?.id}`);
+  if (!userChatDB) return interaction.createMessage("Not yet generated. Try to stay active.");
+
+  return interaction.createMessage({
+    content: `Your point (in order to get Active Members role): **${Math.floor(userChatDB?.probability || 0)}%** out of **${Config.userChat.meta.threshold}%**`
+  });
+};
