5fe0eb2bf65b36e5c00ea1f81ceb5c04d663d8de
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-20 01:06:36 +0100

    added daily user chat manifesto checking

--- factory/CRON.ts
+++ factory/CRON.ts
@@ -5,6 +5,62 @@ import GMDIBot from "../handler/Client";
 import ms from "ms";
 
 export default async function (client: GMDIBot) {
+  // user chat
+  scheduleJob({hour: 0, minute: 0, tz: "Asia/Jakarta"}, async function() {
+    // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    let data: [string, UserChatInterface][] = Object.entries(db.get("userChatRate"));
+
+    for (const [id, content] of data) {
+
+      // push the sum today to sum week, in order to get converted into averages
+      let currentOverload: boolean = false;
+      if (!content.sum.week) {
+        db.set(`userChatRate.${id}.sum.week`, [content.sum.today]);
+      } else {
+        if (content.sum.week.length >= 7) {
+          db.push(`userChatRate.${id}.sum.week`, content.sum.week.slice(1).push(content.sum.today));
+          currentOverload = true;
+        } else {
+          db.push(`userChatRate.${id}.sum.week`, content.sum.today);
+        };
+      };
+
+      // reset
+      db.set(`userChatRate.${id}.sum.today`, 0);
+
+      // calculation
+      let arrayOfAvg = content.sum.week as number[];
+      currentOverload ? arrayOfAvg.slice(1).push(content.sum.today) : arrayOfAvg.push(content.sum.today);
+      let sum = arrayOfAvg.reduce((acc, current) => acc + current);
+      let res = Math.floor(sum / 7); // 7 is day of week
+      db.set(`userChatRate.${id}.probability`, res);
+
+      // pass the eligibility
+      if (res >= Config.userChat.meta.threshold) {
+        client.addGuildMemberRole(Config.guildID, id, Config.role.activeUser);
+      };
+
+      if (arrayOfAvg.length === 7 && res <= Config.userChat.meta.threshold) {
+        if (!content.warning?.current) {
+          let varyObj: UserChatInterface["warning"] = { since: new Date(), current: true };
+          db.set(`userChatRate.${id}.warning`, varyObj);
+        }
+        
+        else if (content.warning?.current && content.warning.since && Math.floor((new Date().getTime() - content.warning.since.getTime()) / 864e5) >= Config.userChat.meta.resetAfterInDay) {
+          client.removeGuildMemberRole(Config.guildID, id, Config.role.activeUser);
+        };
+      }
+
+      else {
+        let varyObj: UserChatInterface["warning"] = { since: null, current: false };
+        db.set(`userChatRate.${id}.warning`, varyObj);
+      };
+
+      continue;
+    };
+  });
+
+  // warning timeout
   scheduleJob('* 1 * * *', async function() {
     if (!db.get("warningLasted")) return;
 
@@ -77,6 +133,7 @@ export default async function (client: GMDIBot) {
     };
   });
 
+  // member welcome message replacement
   scheduleJob('* 12 * * *', async function() {
     if (!db.get("replaceWelcomeMessageUser")) return;
 
@@ -95,6 +152,7 @@ export default async function (client: GMDIBot) {
     };
   });
 
+  // counting error reset
   scheduleJob('30 * * * *', function() {
     client.counter.userError.clear();
   });
