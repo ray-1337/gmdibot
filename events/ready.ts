fac869401b1bf18eb9c78e341aafe5f262ae137a
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-22 03:24:54 +0100

    moderation registry

--- events/ready.ts
+++ events/ready.ts
@@ -1,15 +1,19 @@
 import GMDIBot from "../handler/Client";
 import CRON from "../factory/CRON";
 
-export = async (client: GMDIBot) => {
-  client.editStatus("idle", {type: 3, name: "Love Live"});
+// Moderation Registry
+import removalChannelCooldown from "../moderationRegistry/removalChannelCooldown";
+import slashCommandProceed from "../moderationRegistry/slashCommandProceed";
+
+export default async (client: GMDIBot) => {
+  client.editStatus("idle", {type: 3, name: "Gayshit Impact"});
 
   console.log("Ready.");
 
-  client.emit("removalChannelCooldown", client);
+  removalChannelCooldown(client);
 
   // edit guild command
-  client.emit("slashCommandProceed", client);
+  slashCommandProceed(client);
 
   // cron
   CRON(client);
