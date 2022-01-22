e2d4b29d307997d198a03598c3364b5bdf0c2bca
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-01-22 03:24:08 +0100

    added gmdi server

---
+++ server/Server.ts
@@ -0,0 +1,27 @@
+import nanoexpress from 'nanoexpress';
+import fs from "fs";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = nanoexpress();
+
+app.get('/', (_, res) => {
+  return res.send("ngapain lu liat-liat. kepo ya.");
+});
+
+app.get('/:filename', (req, res) => {
+  if (!req.params?.filename || !fs.existsSync(`../../gmdi-content-logging/${req.params.filename}`)) {
+    res.status(404);
+    return res.send("unknown content");
+  };
+
+  return res.sendFile(`content/${req.params.filename}`);
+});
+
+app.get('/favicon.ico', (_, res) => {
+  res.status(206);
+  return res.end();
+});
+
+export default app.listen(process.env.SERVER_PORT as unknown as number).catch(console.error);
