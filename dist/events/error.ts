f531ea7d6e3d62b79f094868d442bdc1893ae239
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-02-20 19:58:52 +0100

    bit of good way to check some err code.

--- dist/events/error.ts
+++ dist/events/error.ts
@@ -1,5 +1,4 @@
 export default (client, err) => {
-  let errorCode: number[] = [1001, 1006];
-  if (errorCode.includes(err.code)) return;
+  if ([1001, 1006].some(x => x == err.code)) return;
   else return console.error(err);
 };
