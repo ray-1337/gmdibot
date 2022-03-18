8545798a3ba5e9b00bf44c3ad2561696ef89701b
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-03-18 10:34:27 +0100

    added guildMessageReactions intent

--- dist/index.ts
+++ dist/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config({path: process.cwd() + "/.env"});
 
 const client = new GMDIBot(`Bot ${process.env.DISCORD_BOT_TOKEN}`, {
-  intents: ["guilds", "guildMembers", "guildMessages"],
+  intents: ["guilds", "guildMembers", "guildMessages", "guildMessageReactions"],
   getAllUsers: false,
   guildCreateTimeout: 30000,
   messageLimit: 3
