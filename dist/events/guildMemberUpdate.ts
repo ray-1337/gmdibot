49aa1eca9f620486b56268295a2e37e6fd1ff31d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-03-18 10:46:12 +0100

    fixed typing

--- dist/events/guildMemberUpdate.ts
+++ dist/events/guildMemberUpdate.ts
@@ -1,9 +1,10 @@
 import Eris from "eris";
 import config from "../config/config";
+import GMDIBot from "../handler/Client";
 
 import membershipScreenings from "../moderationRegistry/membershipScreenings";
 
-export default async (client: Eris.Client, guild: Eris.Guild, member: Eris.Member, oldMember: Eris.OldMember) => {
+export default async (client: Eris.Client & GMDIBot, guild: Eris.Guild, member: Eris.Member, oldMember: Eris.OldMember) => {
   if (guild.id !== config.guildID || member.bot) return;
 
   // passed Membership Screenings
