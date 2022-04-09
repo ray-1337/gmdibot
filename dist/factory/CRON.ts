e470338715ab75b2921914dc20c6ecfcd3c793c0
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-04-09 20:02:26 +0200

    stop unused cron

--- dist/factory/CRON.ts
+++ dist/factory/CRON.ts
@@ -8,14 +8,14 @@ import WarningTimeout from "./silk/WarningTimeout.FIJI";
 
 export default async function (client: GMDIBot) {
   // user chat
-  scheduleJob({hour: 0, minute: 0, tz: "Asia/Jakarta"}, () => UserChat(client));
+  // scheduleJob({hour: 0, minute: 0, tz: "Asia/Jakarta"}, () => UserChat(client));
 
   // warning timeout
-  scheduleJob('* */1 * * *', () => WarningTimeout(client));
+  // scheduleJob('* */1 * * *', () => WarningTimeout(client));
 
   // member welcome message replacement
-  scheduleJob('* */12 * * *', () => EntranceProposal());
+  // scheduleJob('* */12 * * *', () => EntranceProposal());
 
   // counting error reset
-  scheduleJob('*/30 * * * *', () => CountingErrorReset(client));
+  // scheduleJob('*/30 * * * *', () => CountingErrorReset(client));
 };
