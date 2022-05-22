f14be05f091dd43403f8ca5bae7db8bacc3b060f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-05-22 10:44:49 +0200

    use fs.promises instead

--- dist/handler/Event.ts
+++ dist/handler/Event.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import Eris from "eris";
 
 export default async (client: Eris.Client) => {
-  const events = fs.readdirSync("./dist/events/");
+  const events = await fs.promises.readdir("./dist/events/");
 
   for (const evt of events) client.on(evt.split(".")[0], (...args) => require(`../events/${evt}`).default(client, ...args));
 };
