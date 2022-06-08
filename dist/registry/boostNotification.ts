824fedd97813b5387da2aa3e94763f9412a00917
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-06-08 11:09:41 +0200

    optional chaining just in case

--- dist/registry/boostNotification.ts
+++ dist/registry/boostNotification.ts
@@ -4,7 +4,7 @@ import Config from "../config/config";
 export default async (client: Eris.GMDIExtension, member: Eris.Member, oldMember: Eris.OldMember) => {
   if (member.bot) return;
 
-  if (!oldMember?.premiumSince && member.premiumSince) {
+  if (!oldMember?.premiumSince && member?.premiumSince) {
     const userTag = `${member.username}#${member.discriminator}`;
     const embed = new Eris.RichEmbed().setColor(0xf47fff).setTimestamp()
     .setAuthor(`${userTag} barusan ngeboost server GMDI`, undefined, member.user.dynamicAvatarURL("png", 32))
