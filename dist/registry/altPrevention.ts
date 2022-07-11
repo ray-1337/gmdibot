1f8fdd0167a1aad185041b8f63edc3be62ce0af8
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-07-11 14:58:03 +0200

    error handling in altPrevention

--- dist/registry/altPrevention.ts
+++ dist/registry/altPrevention.ts
@@ -2,14 +2,19 @@ import {GMDIExtension, Guild, Member} from "eris";
 import ms from "ms";
 
 export default async (client: GMDIExtension, guild: Guild, member: Member) => {
-  const day = ms("1d");
-  const age = Date.now() - member.createdAt;
-  const dayLimit = 30; // 30 days
-
-  if (Math.round(age / day) < dayLimit) {
-    await client.kickGuildMember(guild.id, member.id);
-    return true;
-  } else {
+  try {
+    const day = ms("1d");
+    const age = Date.now() - member.createdAt;
+    const dayLimit = 30; // 30 days
+  
+    if (Math.round(age / day) < dayLimit) {
+      await client.kickGuildMember(guild.id, member.id);
+      return true;
+    } else {
+      return false;
+    };
+  } catch (error) {
+    console.error(error);
     return false;
   };
 };
