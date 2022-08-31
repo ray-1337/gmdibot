93b160a0c6edf1eddc5654e515e080482fbaf407
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-08-31 21:23:58 +0200

    wrong alt kick calculation

--- dist/registry/newMemberValidation.ts
+++ dist/registry/newMemberValidation.ts
@@ -11,7 +11,7 @@ export default async (client: GMDIExtension, guild: Guild, member: Member) => {
     const day = ms("1d");
     const age = Date.now() - member.createdAt;
     const dayLimit = 30; // 30 days
-    if (Math.round(age / day) > dayLimit) {
+    if (Math.round(age / day) < dayLimit) {
       await client.kickGuildMember(guild.id, member.id);
     };
 
