1daa370e4e027af0bc125a952e21bac55c9d910b
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-08-31 03:07:40 +0200

    prevent fake boost message

--- dist/registry/boostNotification.ts
+++ dist/registry/boostNotification.ts
@@ -4,7 +4,8 @@ import Config from "../config/config";
 export default async (client: Eris.GMDIExtension, member: Eris.Member, oldMember: Eris.OldMember) => {
   if (member.bot) return;
 
-  if (!oldMember?.premiumSince && member?.premiumSince) {
+  const boostRole = "589643758564540417";
+  if (!oldMember?.premiumSince && member?.premiumSince && !member.roles.includes(boostRole)) {
     const userTag = `${member.username}#${member.discriminator}`;
     const embed = new Eris.RichEmbed().setColor(0xf47fff).setTimestamp()
     .setAuthor(`${userTag} barusan ngeboost server GMDI`, undefined, member.user.dynamicAvatarURL("png", 32))
