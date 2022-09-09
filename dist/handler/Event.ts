49bf74b7b9d140851b6cc9508dab721b3043138e
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-09-09 05:02:38 +0200

    event rebase

--- dist/handler/Event.ts
+++ dist/handler/Event.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
-import Eris from "eris";
+import { GMDIExtension, ClientEvents } from "oceanic.js";
 
-export default async (client: Eris.GMDIExtension) => {
+export default async (client: GMDIExtension) => {
   const events = await fs.promises.readdir("./dist/events/");
 
-  for (const evt of events) client.on(evt.split(".")[0], (...args) => require(`../events/${evt}`).default(client, ...args));
+  for (const evt of events) {
+    client.on(evt.split(".")[0] as keyof ClientEvents, (...args) => require(`../events/${evt}`).default(client, ...args));
+  };
 };
