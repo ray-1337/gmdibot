f568c99d4128004f1ccd3299b478fbcc0fa3c333
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-09-14 18:35:41 +0200

    viprin discrimination (requested by Lipz)

--- dist/events/messageUpdate.ts
+++ dist/events/messageUpdate.ts
@@ -1,7 +1,11 @@
 import {GMDIExtension, Message, AnyGuildTextChannel, JSONMessage} from "oceanic.js";
 
 import ghostPing from "../registry/ghostPing";
+import viprin from "../registry/viprin";
 
 export default async (client: GMDIExtension, message: Message<AnyGuildTextChannel>, oldMessage?: JSONMessage | null) => {
   ghostPing(client, message, oldMessage);
+  
+  // requested by Lipz
+  viprin(client, message);
 };
