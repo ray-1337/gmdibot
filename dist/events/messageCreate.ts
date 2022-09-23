770085005f87ed4efa98ff516b0d6ca0f4861611
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-09-23 03:49:29 +0200

    give member role while talking

--- dist/events/messageCreate.ts
+++ dist/events/messageCreate.ts
@@ -4,6 +4,9 @@ import Config from "../config/config";
 // Moderation Registry
 import ChannelCooldown from "../registry/channelCooldown";
 
+// Forgotten Member Role
+import forgottenMemberRole from "../registry/forgottenMemberRole";
+
 // command
 import EvalFactory from "../registry/eval";
 
@@ -17,6 +20,8 @@ export default async (client: GMDIExtension, message: Message<AnyGuildTextChanne
   if (Config.channel.watchChannelModeration.some(x => x === message.channel.id)) {
     ChannelCooldown(client, message);
   };
+
+  forgottenMemberRole(undefined, message);
 
   let args = message.content.slice(Config.prefix.length).trim().split(/ +/g);
   let cmd = args.shift()?.toLowerCase();
---
+++ dist/registry/forgottenMemberRole.ts
@@ -0,0 +1,21 @@
+import { Message, AnyGuildTextChannel } from "oceanic.js";
+import ms from "ms";
+
+export default async (_, message: Message<AnyGuildTextChannel>) => {
+  const memberRole = "312868594549653514";
+
+  const day = ms("1d"), dayLimit = 30, age = Date.now() - new Date(message.member.createdAt).getTime();
+
+  if (
+    Math.round(age / day) > dayLimit && // pass 30 days discord account age
+    !message.member.pending && // not in verification (membership screenings)
+    !message.member.roles.includes(memberRole) // currently dont have member role
+  ) {
+    try {
+      await message.member.addRole(memberRole, "Belum dikasih.");
+    } catch (error) {
+      console.error(error);
+      return;
+    };
+  };
+};
