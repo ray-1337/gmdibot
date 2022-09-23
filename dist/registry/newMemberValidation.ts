d874a503fc7c912459553f77a5893adcad5abeaf
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-09-23 03:47:13 +0200

    removed kick alt

--- dist/registry/newMemberValidation.ts
+++ dist/registry/newMemberValidation.ts
@@ -1,19 +1,10 @@
-import { GMDIExtension, Guild, Member } from "oceanic.js";
-import ms from "ms";
+import { GMDIExtension, Member } from "oceanic.js";
 import config from "../config/config";
 import usernameModeration from "./usernameModeration";
 
 export default async (client: GMDIExtension, member: Member) => {
   try {
     if (member.guild.id !== config.guildID || member.bot) return;
-    
-    // user alt checking
-    const day = ms("1d");
-    const age = Date.now() - new Date(member.createdAt).getTime();
-    const dayLimit = 30; // 30 days
-    if (Math.round(age / day) < dayLimit) {
-      await client.rest.guilds.removeMember(member.guild.id, member.id);
-    };
 
     // nickname validation
     usernameModeration(client, member);
