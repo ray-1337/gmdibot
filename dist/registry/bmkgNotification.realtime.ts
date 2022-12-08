808694dfae2cd504ab15462b220f2cddbdf277e3
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-12-08 01:58:33 +0100

    increased limit mag. to 4.5

--- dist/registry/bmkgNotification.realtime.ts
+++ dist/registry/bmkgNotification.realtime.ts
@@ -40,7 +40,7 @@ export default async (client: GMDIExtension) => {
       if (cachedEarthQuake && cachedEarthQuake === String(earthquakeID)) return;
       
       // at least >= {specified}
-      let limitMagnitudeToPost = 3.0;
+      let limitMagnitudeToPost = 4.5;
       if (Number(latestEQ.mag._text) < limitMagnitudeToPost) {
         await redis.set(cachedEQKey, earthquakeID);
         return console.log(`GMDI & BMKG (realtime alternative): Posted with ID_${earthquakeID} but lower mag; ${latestEQ.mag._text}`);
