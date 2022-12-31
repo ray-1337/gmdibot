34407f670c8becce8ac893aa3a2f8e5839169d1f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-12-31 06:52:21 +0100

    reactivate welcome goodbye message

--- dist/events/guildMemberRemove.ts
+++ dist/events/guildMemberRemove.ts
@@ -3,10 +3,10 @@ import config from "../config/config";
 import ms from "ms";
 
 export default async (client: GMDIExtension, member: User | Member, guild: Guild) => {
-  // if (guild.id !== config.guildID || member.bot) return;
+  if (guild.id !== config.guildID || member.bot) return;
 
   // skip if member still on verification pending
-  // if (member instanceof Member && member.pending) return;
+  if (member instanceof Member && member.pending) return;
 
   // client.database.set(`replaceWelcomeMessageUser.${member.id}`, {
   //   activateWhenComingBack: true,
@@ -16,18 +16,18 @@ export default async (client: GMDIExtension, member: User | Member, guild: Guild
 
   // Embed
   // let embeds = new Eris.RichEmbed().setColor(0xC82427).setTimestamp();
-  // let embed: EmbedOptions = {
-  //   color: 0xC82427,
-  //   timestamp: new Date().toISOString()
-  // };
+  let embed: EmbedOptions = {
+    color: 0xC82427,
+    timestamp: new Date().toISOString()
+  };
 
-  // if (member instanceof Member && Math.floor(Date.now() - new Date(member.joinedAt!).getTime()) < ms("5m")) {
-  //   embed = {...embed, ...{title: "Dadah...", description: `**${member.username}#${member.discriminator}** langsung keluar dari server.`}};
-  // }
+  if (member instanceof Member && Math.floor(Date.now() - new Date(member.joinedAt!).getTime()) < ms("5m")) {
+    embed = {...embed, ...{title: "Dadah...", description: `**${member.username}#${member.discriminator}** langsung keluar dari server.`}};
+  }
 
-  // else {
-  //   embed = {...embed, ...{title: "Farewell...", description: `**${member.username}#${member.discriminator}** keluar dari server.`}};
-  // };
+  else {
+    embed = {...embed, ...{title: "Farewell...", description: `**${member.username}#${member.discriminator}** keluar dari server.`}};
+  };
   
-  // return client.rest.channels.createMessage(config.channel.general, {embeds: [embed]});
+  return client.rest.channels.createMessage(config.channel.general, {embeds: [embed]});
 };
