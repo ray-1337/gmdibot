abab453e808461f8f7ffdc3d443390b17f5c44c7
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-01-27 16:19:57 +0100

    specific log

--- dist/registry/freedom.ts
+++ dist/registry/freedom.ts
@@ -33,7 +33,8 @@ export default async (client: GMDIExtension, message: Message<AnyGuildTextChanne
     // delete the message
     async function deleteContent() {
       try {
-        console.log("Deleted ", message.content)
+        console.log(`Deleted ${message.content} on ${message.channelID}`);
+        
         await client.rest.channels.deleteMessage(message.channelID, message.id);
 
         // add punishment count
