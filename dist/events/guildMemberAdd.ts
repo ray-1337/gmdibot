64ad5ce2992132aa823479d143f4582719d4d5a1
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-02-25 13:19:33 +0100

    bring back alt account prevention

--- dist/events/guildMemberAdd.ts
+++ dist/events/guildMemberAdd.ts
@@ -1,10 +1,12 @@
 import { GMDIExtension, Member } from "oceanic.js";
 import config from "../config/config";
 import newMemberValidation from "../registry/newMemberValidation";
+import altPrevention from "../registry/altPrevention";
 
 export default async (client: GMDIExtension, member: Member) => {
   if (member.guild.id !== config.guildID || member.bot) return;
 
-  newMemberValidation(client, member);
-  return;
+  if (!(await altPrevention(client, member.guild, member))) {
+    newMemberValidation(client, member);
+  };
 };
---
+++ dist/registry/altPrevention.ts
@@ -0,0 +1,20 @@
+import {GMDIExtension, Guild, Member} from "oceanic.js";
+import ms from "ms";
+
+export default async (client: GMDIExtension, guild: Guild, member: Member) => {
+  try {
+    const day = ms("1d");
+    const age = Date.now() - new Date(member.createdAt).getTime();
+    const dayLimit = 30; // 30 days
+  
+    if (Math.round(age / day) < dayLimit) {
+      await client.rest.guilds.removeMember(guild.id, member.id);
+      return true;
+    } else {
+      return false;
+    };
+  } catch (error) {
+    console.error(error);
+    return false;
+  };
+};
