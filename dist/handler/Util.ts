63c018c92c6c7c2f8628cfdbab556822e88a8eda
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-03-31 10:58:07 +0200

    added shuffle func to utility

--- dist/handler/Util.ts
+++ dist/handler/Util.ts
@@ -66,3 +66,23 @@ export function getRandomInt(min: number, max: number) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export function shuffle<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  let randomIndex: number;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]
+    ];
+  }
+
+  return array;
+};
