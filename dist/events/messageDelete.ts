4cd18464f5b913299d9582cd5318027051a915bb
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-04-17 13:57:14 +0200

    [draft] disable contentLogging

--- dist/events/messageDelete.ts
+++ dist/events/messageDelete.ts
@@ -2,11 +2,11 @@ import {GMDIExtension, Message, Uncached, AnyGuildTextChannel} from "oceanic.js"
 
 export type PossiblyUncachedMessage = Message<AnyGuildTextChannel> | { channel: AnyGuildTextChannel | Uncached; id: string };
 
-import contentLogging from "../registry/contentLogging";
+// import contentLogging from "../registry/contentLogging";
 import ghostPing from "../registry/ghostPing";
 
 export default async (client: GMDIExtension, message: PossiblyUncachedMessage) => {
-  contentLogging(client, message);
+  // contentLogging(client, message);
 
   ghostPing(client, message);
 };
