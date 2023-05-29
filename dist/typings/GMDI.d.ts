b1214a53006c7e20521d9fae9095bb86ae246517
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-05-29 15:25:54 +0200

    remove unused typings

--- dist/typings/GMDI.d.ts
+++ dist/typings/GMDI.d.ts
@@ -1,19 +1,3 @@
-interface FarewellMemberConclusion {
-  activateWhenComingBack: boolean;
-  leavingSince: number;
-  memberID: string;
-}
-
-interface WarningLastedOptions {
-  since?: number | Date;
-  due?: number | Date;
-  full?: number;
-  memberID: string;
-  previousRoleList?: string[],
-  level: 1 | 2 | 3; // specific level
-  warningLogID: string
-}
-
 interface DeletedMessage {
   id: string;
   guildID: string;
