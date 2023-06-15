ec2e10a92ffcce04e0c01c06756c4fc7fbeb4ccb
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-06-15 05:22:39 +0200

    add utility to client class

--- dist/handler/Client.ts
+++ dist/handler/Client.ts
@@ -1,12 +1,15 @@
 import {Client, ClientOptions} from "oceanic.js";
 import jsoning from "jsoning";
+import * as Utility from "./Util";
 
 export default class GMDIBot extends Client {
   database: jsoning;
+  utility: typeof Utility;
 
   constructor(options: ClientOptions) {
     super(options);
 
     this.database = new jsoning(process.cwd() + "/database/db.json");
+    this.utility = Utility;
   };
 };
