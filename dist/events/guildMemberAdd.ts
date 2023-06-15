bb30481934cec85668a2e536c64e51c67ff362cf
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-06-15 05:21:35 +0200

    remove newMemberValidation

--- dist/events/guildMemberAdd.ts
+++ dist/events/guildMemberAdd.ts
@@ -1,12 +1,9 @@
 import { GMDIExtension, Member } from "oceanic.js";
 import { gmdiGuildID } from "../handler/Config";
-import newMemberValidation from "../registry/newMemberValidation";
 import altPrevention from "../registry/altPrevention";
 
 export default async (client: GMDIExtension, member: Member) => {
   if (member.guild.id !== gmdiGuildID || member.bot) return;
 
-  if (!(await altPrevention(client, member.guild, member))) {
-    newMemberValidation(client, member);
-  };
+  return await altPrevention(client, member.guild, member);
 };
