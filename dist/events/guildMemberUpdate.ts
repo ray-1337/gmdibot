d75dcd0024539f3ee5bb7c977dfcfea64b34cfb3
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-06-15 05:15:40 +0200

    deprecate username moderation feature

--- dist/events/guildMemberUpdate.ts
+++ dist/events/guildMemberUpdate.ts
@@ -1,15 +1,11 @@
 import { Member, GMDIExtension, JSONMember } from "oceanic.js";
 
 import membershipScreenings from "../registry/membershipScreenings";
-import usernameModeration from "../registry/usernameModeration";
 import boostNotification from "../registry/boostNotification"
 
 export default async (client: GMDIExtension, member: Member, oldMember: JSONMember | null) => {
   // passed Membership Screenings
   membershipScreenings(client, member, oldMember);
-
-  // username moderation
-  usernameModeration(client, member);
 
   // boost notification
   boostNotification(client, member, oldMember);
--- dist/registry/newMemberValidation.ts
+++ dist/registry/newMemberValidation.ts
@@ -1,13 +1,9 @@
 import { GMDIExtension, Member } from "oceanic.js";
 import { gmdiGuildID } from "../handler/Config";
-import usernameModeration from "./usernameModeration";
 
 export default async (client: GMDIExtension, member: Member) => {
   try {
     if (member.guild.id !== gmdiGuildID || member.bot) return;
-
-    // nickname validation
-    usernameModeration(client, member);
 
     return;
   } catch (error) {
