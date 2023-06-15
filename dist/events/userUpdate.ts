bc9085a883d606fa1fbcf742c2f06524af3ce2c6
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-06-15 05:27:15 +0200

    deprecate username moderation

--- dist/events/userUpdate.ts
+++
@@ -1,8 +0,0 @@
-import {GMDIExtension, User} from "oceanic.js";
-
-import usernameModeration from "../registry/usernameModeration";
-
-export default async (client: GMDIExtension, user: User, /*oldUser: PartialUser*/) => {
-  // username moderation
-  usernameModeration(client, user);
-};
