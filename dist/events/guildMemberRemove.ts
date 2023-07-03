6e6f5ae267a5add23105bd849e1726664f723d5d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-07-03 15:01:31 +0200

    guildMemberRemove code cleanup

--- dist/events/guildMemberRemove.ts
+++ dist/events/guildMemberRemove.ts
@@ -1,4 +1,4 @@
-import { GMDIExtension, Guild, Member, User, EmbedOptions } from "oceanic.js";
+import { GMDIExtension, Guild, Member, User } from "oceanic.js";
 import { gmdiGuildID, firstGeneralTextChannelID } from "../handler/Config";
 import ms from "ms";
 import { EmbedBuilder as RichEmbed } from "@oceanicjs/builders";
@@ -9,18 +9,8 @@ export default async (client: GMDIExtension, member: User | Member, guild: Guild
   // skip if member still on verification pending
   if (member instanceof Member && member.pending) return;
 
-  // client.database.set(`replaceWelcomeMessageUser.${member.id}`, {
-  //   activateWhenComingBack: true,
-  //   leavingSince: Date.now(),
-  //   memberID: member.id
-  // });
-
   // Embed
   let embed = new RichEmbed().setColor(0xC82427).setTimestamp(new Date());
-  // let embed: EmbedOptions = {
-  //   color: 0xC82427,
-  //   timestamp: new Date().toISOString()
-  // };
 
   if (member instanceof Member && Math.floor(Date.now() - new Date(member.joinedAt!).getTime()) < ms("5m")) {
     embed.setTitle("Dadah...").setDescription(`**${client.utility.usernameHandle(member)}** langsung keluar dari server.`);
