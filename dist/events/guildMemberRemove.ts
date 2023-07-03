07948eaa27ba56d3f791d10673062c346856517d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-07-03 15:02:39 +0200

    deprecate leave embed message sooner

--- dist/events/guildMemberRemove.ts
+++ dist/events/guildMemberRemove.ts
@@ -1,6 +1,5 @@
 import { GMDIExtension, Guild, Member, User } from "oceanic.js";
 import { gmdiGuildID, firstGeneralTextChannelID } from "../handler/Config";
-import ms from "ms";
 import { EmbedBuilder as RichEmbed } from "@oceanicjs/builders";
 
 export default async (client: GMDIExtension, member: User | Member, guild: Guild) => {
@@ -10,15 +9,13 @@ export default async (client: GMDIExtension, member: User | Member, guild: Guild
   if (member instanceof Member && member.pending) return;
 
   // Embed
-  let embed = new RichEmbed().setColor(0xC82427).setTimestamp(new Date());
-
-  if (member instanceof Member && Math.floor(Date.now() - new Date(member.joinedAt!).getTime()) < ms("5m")) {
-    embed.setTitle("Dadah...").setDescription(`**${client.utility.usernameHandle(member)}** langsung keluar dari server.`);
-  }
-
-  else {
-    embed.setTitle("Farewell...").setDescription(`**${client.utility.usernameHandle(member)}** keluar dari server.`);
-  };
+  const embed = new RichEmbed()
+  .setColor(0xC82427)
+  .setTimestamp(new Date())
+  .setTitle("Farewell...")
+  .setDescription(`**${client.utility.usernameHandle(member)}** keluar dari server.`);
   
-  return client.rest.channels.createMessage(firstGeneralTextChannelID, {embeds: embed.toJSON(true)});
+  return client.rest.channels.createMessage(firstGeneralTextChannelID, {
+    embeds: embed.toJSON(true)
+  });
 };
