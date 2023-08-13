43aa6a7c566c88cf586512d42780245549cec8ef
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-08-13 12:30:11 +0200

    remove jsoning

--- dist/handler/Client.ts
+++ dist/handler/Client.ts
@@ -1,15 +1,12 @@
 import {Client, ClientOptions} from "oceanic.js";
-import jsoning from "jsoning";
 import * as Utility from "./Util";
 
 export default class GMDIBot extends Client {
-  database: jsoning;
   utility: typeof Utility;
 
   constructor(options: ClientOptions) {
     super(options);
 
-    this.database = new jsoning(process.cwd() + "/database/db.json");
     this.utility = Utility;
   };
 };
