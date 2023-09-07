123594b12429bee9bc358bfed576c760e8363a33
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-09-07 04:44:04 +0200

    simplify guard code without bracket

--- src/registry/starboard.ts
+++ src/registry/starboard.ts
@@ -67,100 +67,100 @@ export default async (client: GMDIExtension, msg: Message<AnyTextableGuildChanne
     if (reactions.find(val => message && (message.author.id == val.id))) ++limit;
 
     // check if the starboard has been posted before or nah
-    if (message.reactions[starEmoji].count >= limit) {
-      const table = await starboardStorage.tableAsync("posted");
+    if (message.reactions[starEmoji].count < limit) return;
+    
+    const table = await starboardStorage.tableAsync("posted");
 
-      if ((await table.has(message.id))) {
-        return;
-      };
+    if ((await table.has(message.id))) {
+      return;
+    };
 
-      const userTag = `${client.utility.usernameHandle(message.author)}`;
-      let embed = new RichEmbed().setColor(0xffac33).setTimestamp(new Date(message.timestamp))
+    const userTag = `${client.utility.usernameHandle(message.author)}`;
+    let embed = new RichEmbed().setColor(0xffac33).setTimestamp(new Date(message.timestamp))
       .setDescription(truncate(message.content, 1024))
       .setAuthor(`${userTag} (${message.author.id})`, message.author.avatarURL("png", 16))
 
-      if (starterQuery) {
-        const firstReactUserID = await firstReactedTable.get(message.id);
-        const starterUser = client.users.get(firstReactUserID) || await client.rest.users.get(firstReactUserID).catch(() => { });
-        if (starterUser) {
-          embed.setFooter(`Si Pemulai: ${client.utility.usernameHandle(starterUser)}`)
-        };
+    if (starterQuery) {
+      const firstReactUserID = await firstReactedTable.get(message.id);
+      const starterUser = client.users.get(firstReactUserID) || await client.rest.users.get(firstReactUserID).catch(() => { });
+      if (starterUser) {
+        embed.setFooter(`Si Pemulai: ${client.utility.usernameHandle(starterUser)}`)
       };
-
-      let file: File | undefined;
-
-      let embeddings: string[] = [];
-
-      if (message.attachments.size || message.embeds.length) {
-        let ext = {
-          "image/gif": ".gif",
-          "image/jpeg": ".jpeg",
-          "image/jpg": ".jpg",
-          "image/png": ".png",
-          "image/webp": ".webp",
-          "video/mp4": ".mp4",
-          "video/webm": ".webm"
-        };
-
-        // attachments
-        if (message.attachments.size == 1) {
-          if (message.attachments.toArray()[0].contentType?.match(/^(image\/(jpe?g|gif|png|webp))/gi)) {
-            embed.setImage(normalizeURL(message.attachments.toArray()[0].url));
-          }
-
-          else if (message.attachments.toArray()[0].contentType?.match(/^(video)/gi)) {
-            embeddings.push(normalizeURL(message.attachments.toArray()[0].url));
-          };
-        } else if (message.attachments.size > 1) {
-          for (let data of message.attachments) {
-            if (!data[1].contentType) continue;
-
-            if (Object.keys(ext).find(mime => mime == data[1].contentType)) {
-              embeddings.push(data[1].url);
-            };
-
-            continue;
-          };
-        };
-
-        // embeds
-        if (message.embeds.length == 1) {
-          if (message.embeds[0].type == "image" && message.embeds[0].url) {
-            embed.setImage(normalizeURL(message.embeds[0].url));
-          }
-
-          else if (message.embeds[0].type == "video" && message.embeds[0].url) {
-            embeddings.push(normalizeURL(message.embeds[0].url));
-          };
-        } else if (message.embeds.length > 1) {
-          for (let data of message.embeds) {
-            if (data[1].type.match(/(image|video)/gi) && data.url) {
-              embeddings.push(data.url);
-            };
-
-            continue;
-          };
-        };
-      };
-
-      let redirectButton: MessageActionRow[] = [{
-        type: Constants.ComponentTypes.ACTION_ROW,
-        components: [{
-          type: Constants.ComponentTypes.BUTTON,
-          style: Constants.ButtonStyles.LINK,
-          label: "Dokumen Asli",
-          url: `https://discord.com/channels/${message.channel.guildID}/${message.channel.id}/${message.id}`
-        }]
-      }];
-
-      await client.rest.channels.createMessage(channelID, {
-        embeds: embed.toJSON(true),
-        components: redirectButton,
-        files: file ? [file] : undefined
-      });
-
-      return await table.set(message.id, true);
     };
+
+    let file: File | undefined;
+
+    let embeddings: string[] = [];
+
+    if (message.attachments.size || message.embeds.length) {
+      let ext = {
+        "image/gif": ".gif",
+        "image/jpeg": ".jpeg",
+        "image/jpg": ".jpg",
+        "image/png": ".png",
+        "image/webp": ".webp",
+        "video/mp4": ".mp4",
+        "video/webm": ".webm"
+      };
+
+      // attachments
+      if (message.attachments.size == 1) {
+        if (message.attachments.toArray()[0].contentType?.match(/^(image\/(jpe?g|gif|png|webp))/gi)) {
+          embed.setImage(normalizeURL(message.attachments.toArray()[0].url));
+        }
+
+        else if (message.attachments.toArray()[0].contentType?.match(/^(video)/gi)) {
+          embeddings.push(normalizeURL(message.attachments.toArray()[0].url));
+        };
+      } else if (message.attachments.size > 1) {
+        for (let data of message.attachments) {
+          if (!data[1].contentType) continue;
+
+          if (Object.keys(ext).find(mime => mime == data[1].contentType)) {
+            embeddings.push(data[1].url);
+          };
+
+          continue;
+        };
+      };
+
+      // embeds
+      if (message.embeds.length == 1) {
+        if (message.embeds[0].type == "image" && message.embeds[0].url) {
+          embed.setImage(normalizeURL(message.embeds[0].url));
+        }
+
+        else if (message.embeds[0].type == "video" && message.embeds[0].url) {
+          embeddings.push(normalizeURL(message.embeds[0].url));
+        };
+      } else if (message.embeds.length > 1) {
+        for (let data of message.embeds) {
+          if (data[1].type.match(/(image|video)/gi) && data.url) {
+            embeddings.push(data.url);
+          };
+
+          continue;
+        };
+      };
+    };
+
+    let redirectButton: MessageActionRow[] = [{
+      type: Constants.ComponentTypes.ACTION_ROW,
+      components: [{
+        type: Constants.ComponentTypes.BUTTON,
+        style: Constants.ButtonStyles.LINK,
+        label: "Dokumen Asli",
+        url: `https://discord.com/channels/${message.channel.guildID}/${message.channel.id}/${message.id}`
+      }]
+    }];
+
+    await client.rest.channels.createMessage(channelID, {
+      embeds: embed.toJSON(true),
+      components: redirectButton,
+      files: file ? [file] : undefined
+    });
+
+    return await table.set(message.id, true);
   } catch (error) {
     return console.error(error);
   }
