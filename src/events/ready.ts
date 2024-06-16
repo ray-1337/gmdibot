6fb5280754d91736194b2d4aac0617ffd9613b51
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-06-16 13:07:07 +0800

    delete channel cooldown registry

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,9 +5,6 @@ import { version } from "../../package.json";
 // import prayerTiming from "../registry/prayerTiming";
 import bmkgNotificationRealtime from "../registry/bmkgNotification.realtime";
 
-// Moderation Registry
-import removalChannelCooldown from "../registry/removalChannelCooldown";
-
 let isReady: boolean = false;
 
 export default async (client: Client) => {
@@ -22,8 +19,6 @@ export default async (client: Client) => {
     type: Constants.ActivityTypes.LISTENING,
     name: "Dosen"
   }]);
-
-  removalChannelCooldown(client);
 
   // prayerTiming(client);
 
