c4b88ba6f4b7f239d8460c3839ecb6c73f50f211
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-07-12 17:49:22 +0800

    remove dayjs plugin installation from prayer timing registry

--- src/registry/prayerTiming.ts
+++ src/registry/prayerTiming.ts
@@ -1,21 +1,9 @@
+import dayjs from "dayjs";
 import { Client } from "oceanic.js";
 import { EmbedBuilder as RichEmbed } from "@oceanicjs/builders";
 import { request } from "undici";
 import nodeSchedule from "node-schedule";
 import { shuffle } from "../handler/Util";
-
-import dayjs from "dayjs";
-import dayjsUTC from "dayjs/plugin/utc";
-import dayjsTimezone from 'dayjs/plugin/timezone';
-import dayjsLocalize from 'dayjs/plugin/localizedFormat';
-import dayjsCustomParseFormat from 'dayjs/plugin/customParseFormat';
-import dayjsSameBefore from "dayjs/plugin/isSameOrBefore"
-
-dayjs.extend(dayjsTimezone);
-dayjs.extend(dayjsUTC);
-dayjs.extend(dayjsLocalize);
-dayjs.extend(dayjsCustomParseFormat);
-dayjs.extend(dayjsSameBefore);
 
 const endpointVersion: number = 2;
 
