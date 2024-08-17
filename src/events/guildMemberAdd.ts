2f0c72d8f6615f5f4b325c91f5180e0e4d92174e
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-17 14:10:29 +0800

    [draft] temporarily disable alt prevention

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,9 +1,9 @@
 import { Client, Member } from "oceanic.js";
 import { gmdiGuildID } from "../handler/Config";
-import altPrevention from "../registry/altPrevention";
+// import altPrevention from "../registry/altPrevention";
 
 export default async (client: Client, member: Member) => {
   if (member.guild.id !== gmdiGuildID || member.bot) return;
 
-  return await altPrevention(client, member.guild, member);
+  // return await altPrevention(client, member.guild, member);
 };
