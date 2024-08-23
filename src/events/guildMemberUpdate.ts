306e23a7c009a964f27fe56814135d997f07bb5c
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-23 13:52:22 +0800

    remove membershipScreenings from guildMemberUpdate event

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,14 +1,10 @@
 import { Member, Client, JSONMember } from "oceanic.js";
 
-import membershipScreenings from "../registry/membershipScreenings";
 import boostNotification from "../registry/boostNotification";
 import usernameModeration from "../registry/usernameModeration";
 import birthdayRoleControl from "../registry/birthdayRole";
 
 export default async (client: Client, member: Member, oldMember: JSONMember | null) => {
-  // passed Membership Screenings
-  membershipScreenings(client, member, oldMember);
-
   // boost notification
   boostNotification(client, member, oldMember);
 
