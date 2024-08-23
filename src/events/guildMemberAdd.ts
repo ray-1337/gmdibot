27ec0954c0969eb9ef20358b011b9b1a3cdab886
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-24 07:47:04 +0800

    add unverified role for new member

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,9 +1,11 @@
 import { Client, Member } from "oceanic.js";
-import { gmdiGuildID } from "../handler/Config";
-// import altPrevention from "../registry/altPrevention";
 
-export default async (client: Client, member: Member) => {
+import { gmdiGuildID, unverifiedRoleID } from "../handler/Config";
+
+export default async (_: Client, member: Member) => {
   if (member.guild.id !== gmdiGuildID || member.bot) return;
 
-  // return await altPrevention(client, member.guild, member);
+  member.addRole(unverifiedRoleID, "[GMDIBot] New member given unverified role");
+
+  return;
 };
