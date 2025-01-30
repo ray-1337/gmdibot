838e2e91306beedd20b6838029b0e51152832a30
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-01-31 05:42:10 +0800

    move "verification_staff_rejection" to "userPostVerifyModeratorReject" registry

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -74,75 +74,8 @@ export default async (client: Client, interaction: AnyInteractionGateway) => {
         };
 
         case "verification_staff_rejection": {
-          await interaction.defer(64);
-
-          const [channelID, messageID] = interaction.data.customID.split(/\-/gim).slice(1);
-          if (!channelID.match(/(\d{16,21})/gim) || !messageID.match(/(\d{16,21})/gim)) {
-            return interaction.createFollowup({ content: "Invalid secondary ID from interaction.", flags: 64 });
-          };
-
-          const reason = interaction.data.components.getTextInput("reason", true);
-          if (!reason?.length) {
-            return interaction.createFollowup({ content: "Invalid reason.", flags: 64 });
-          };
-
-          const logMessage = await client.rest.channels.getMessage(channelID, messageID);
-          const embed = logMessage?.embeds?.[0] || null;
-          if (!logMessage || !embed || !embed?.author) {
-            return interaction.createFollowup({ content: "Unable to fetch previous log message.", flags: 64 });
-          };
-
-          const userID = embed?.author?.name.match(/(\d{15,21})/gim);
-          if (!userID?.[0]) {
-            return interaction.createFollowup({content: "Unable to fetch user ID from previous embed.", flags: 64});
-          };
-
-          let fields: EmbedField[] = [...embed?.fields || []];
-
-          fields = fields.concat([
-            {
-              name: "Rejection Reason",
-              value: reason
-            },
-            {
-              name: "Rejected by",
-              value: `@${interaction.user.username} (${interaction.user.id})`
-            }
-          ]);
-
-          await client.rest.channels.editMessage(channelID, messageID, {
-            components: [{
-              type: ComponentTypes.ACTION_ROW,
-              components: [
-                logMessage.components[0].components[logMessage.components[0].components.length - 1]
-              ]
-            }],
-
-            embeds: [{
-              ...embed,
-              color: 0xeb4634,
-              title: "New User Verification (Rejected)",
-              timestamp: new Date().toISOString(),
-              fields
-            }]
-          });
-
-          try {
-            const channel = await client.rest.channels.createDM(userID[0]);
-
-            await channel.createMessage({
-              content: stripIndents(`
-                Mohon maaf, verifikasi Anda untuk bergabung ke server Discord kami ditolak dengan alasan berikut.
-                > *${reason}*
-              `)
-            });
-          } catch {};
-
-          await userDoc.set({ userID: userID[0], lastUpdatedAt: Date.now() }, { merge: true });
-          
-          await interaction.createFollowup({content: "Rejected.", flags: 64});
-
-          return;
+          const registry = await import("@/registry/verification/interaction/userPostVerifyModeratorReject");
+          return registry.default(interaction);
         };
       };
     };
