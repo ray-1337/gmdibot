d1a171b5a10783758052f41658e7e579520c9433
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-01-31 05:40:21 +0800

    move "verification_self_buttonclick" to "verifyButtonClick" function

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -44,87 +44,8 @@ export default async (client: Client, interaction: AnyInteractionGateway) => {
       if (interaction.data.componentType === ComponentTypes.BUTTON) {
         switch (interaction.data.customID) {
           case "verification_self_buttonclick": {
-            try {
-              // check if the user is currently has ongoing session
-              if (cache.has(interaction.user.id)) {
-                return interaction.createMessage({
-                  content: "Kamu saat ini memiliki sesi verifikasi yang sedang berjalan. Mohon diselesaikan terlebih dahulu.",
-                  flags: 64
-                });
-              };
-
-              // check if the user is in cooldown state
-              if (cooldown.has(interaction.user.id)) {
-                const currentCooldown = cooldown.get(interaction.user.id);
-
-                if (typeof currentCooldown === "number" && ((Date.now() - currentCooldown) <= cooldownTimeState)) {
-                  const timeRemaining = parseDuration(String(cooldownTimeState - (Date.now() - currentCooldown)), "second") || 0;
-
-                  return interaction.createMessage({
-                    content: `Harap tunggu, kamu masih memiliki cooldown selama **${Math.round(timeRemaining <= 0 ? 0 : timeRemaining)}** detik.`,
-                    flags: 64
-                  });
-                };
-              };
-
-              const currentUser = await userDoc.get();
-              const currentUserState = currentUser.data() as RegisteredUserState;
-
-              switch (true) {
-                // check if the user is blacklisted
-                case (currentUserState?.blacklisted === true): {
-                  return interaction.createMessage({
-                    content: "Maaf, saat ini kamu berada di dalam daftar blacklist. Silakan hubungi staf GMDI untuk informasi lebih lanjut.",
-                    flags: 64
-                  });
-                };
-
-                // check if the user is already verified
-                case (currentUserState?.verified === true): {
-                  if (process.env.npm_lifecycle_event !== "dev") {
-                    await interaction.createMessage({
-                      content: "Kamu sudah terverifikasi.", flags: 64
-                    });
-
-                    await Promise.all([
-                      client.rest.guilds.addMemberRole(gmdiGuildID, interaction.user.id, memberRoleID, "[GMDIBot] Already verified from store"),
-                      client.rest.guilds.removeMemberRole(gmdiGuildID, interaction.user.id, unverifiedRoleID, "[GMDIBot] Already verified from store")
-                    ]);
-
-                    return;
-                  };
-
-                  break;
-                };
-
-                default: break;
-              };
-
-              // check if the user has already submitted the form and it's not verified yet
-              // the user can resubmit if the form is nowhere to be found around at least 10 messages
-              const messages = await client.rest.channels.getMessages(verificationLogChannelID, {
-                filter: (message) => message.author.id === client.user.id &&
-                  message.embeds.length >= 1 &&
-                  message.embeds?.[0].author?.name?.match(interaction.user.id) !== null &&
-                  message.embeds?.[0].title === "New User Verification",
-                limit: 50
-              });
-              
-              if (messages.length >= 1) {
-                return interaction.createMessage({
-                  flags: 64,
-                  content: "Kamu sudah mengirimkan formulir verifikasi. Mohon untuk menunggu hasil verifikasi kurang lebih 24 jam ke depan."
-                });
-              };
-            } catch (error) {
-              console.error(error);
-            };
-
-            return await interaction.createModal({
-              title: "GMDI Server Verification",
-              customID: "verification_self_modal",
-              components: questions
-            });
+            const registry = await import("@/registry/verification/interaction/verifyButtonClick");
+            return registry.default(interaction);
           };
 
           case "gd-verification-cancel":
