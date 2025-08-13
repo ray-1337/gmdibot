599387eb9654d261836e2b16b1da5e1096b72a85
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-08-13 13:27:48 +0800

    Exclude earthquake notification from Papua and/or Nusa Tenggara

--- src/registry/bmkgNotification.ts
+++ src/registry/bmkgNotification.ts
@@ -121,6 +121,11 @@ export default async (client: Client) => {
         };
 
         if (typeof json.display_name === "string") {
+          const lowercaseDisplayName = json.display_name.toLowerCase();
+          if (lowercaseDisplayName.match(/(papua|nusa tenggara)/gim) !== null) {
+            return;
+          };
+
           displayName = json.display_name;
         };
       };
