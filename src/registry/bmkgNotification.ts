9f9b035f9c3878e1716bfab045d3e930ad3e2d98
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-10-21 21:42:53 +0800

    Simplify BMKG notification embed author label

--- src/registry/bmkgNotification.ts
+++ src/registry/bmkgNotification.ts
@@ -103,7 +103,7 @@ export default async (client: Client) => {
         .setTimestamp("now")
         .setColor(earthquakeColor)
         .setURL(`https://www.google.com/maps/search/?api=1&query=${coordinates}`)
-        .setAuthor("Indonesia Tsunami Early Warning System (sub-alternative of BMKG)", "https://indonesiaexpat.id/wp-content/uploads/2022/02/WRS.png", "https://inatews.bmkg.go.id/")
+        .setAuthor("InaTEWS/BMKG", "https://indonesiaexpat.id/wp-content/uploads/2022/02/WRS.png", "https://inatews.bmkg.go.id/")
         .setFooter("Provided by BMKG");
 
       let displayName = data.area;
