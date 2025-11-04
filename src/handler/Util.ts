5ed9b599cf999919d931407f31753c53c2d14093
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-11-05 00:05:57 +0800

    Major tweaks on functions in utility file - Deprecate "countString" and "generateHash" functions - Fix "shuffle" function type error - Use "randomInt" instead "Math.random()" for shuffling array

--- src/handler/Util.ts
+++ src/handler/Util.ts
@@ -1,5 +1,5 @@
 import { Client, Message, Uncached, AnyTextableGuildChannel, Member, PossiblyUncachedMessage } from "oceanic.js";
-import { randomBytes, randomInt } from "crypto";
+import { randomInt } from "crypto";
 
 export const isDevMode = process.env.npm_lifecycle_event === "dev";
 
@@ -41,43 +41,17 @@ export function usernameHandle(user: Member | Member["user"]) {
   return user.discriminator === "0" ? `@${user.username}` : `${user.username}#${user.discriminator}`;
 };
 
-export function countString(string: string) {
-  let freq = {};
-
-  for (let i = 0; i < string.length; i++) {
-    let char = string.charAt(i);
-    freq[char] ? freq[char]++ : freq[char] = 1;
-  };
-
-  return Object.keys(freq).length >= 1 ? freq : null;
-};
-
-export function generateHash(length: number) {
-  return randomBytes(length / 2).toString("hex");
-};
-
 export function truncate(str: string, len: number) {
   return (str.length >= len) ? str.substring(0, len - 1) + '...' : str;
 };
 
 export function shuffle<T>(array: T[]): T[] {
-  let currentIndex = array.length;
-  let randomIndex: number;
+  for (let index = array.length - 1; index > 0; index--) {
+		const newIndex = randomInt(0, (index + 1));
+		[array[index], array[newIndex]] = [array[newIndex] as T, array[index] as T];
+	};
 
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]
-    ];
-  }
-
-  return array;
+	return array;
 };
 
 export function randomNumber(min: number, max: number) {
