b648ba3d44570a69c8acbb8f1e0171f91a463589
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-11-05 14:08:21 +0800

    Major tweaks on username moderation - Instead of using randomized value of base64, use common given names in Indonesia such as Bambang, Joko, Aditya, etc. - Simplify code logic

--- src/registry/usernameModeration.ts
+++ src/registry/usernameModeration.ts
@@ -1,38 +1,64 @@
 import type { Member, JSONMember, Client } from "oceanic.js";
-import { randomBytes } from "node:crypto";
-import { gmdiGuildID } from "../handler/Config";
+
+import { randomNumber } from "@/handler/Util";
+import { gmdiGuildID } from "@/handler/Config";
+
+const cachedSurnames = new Set<string>();
+const fallbackName: string = "biar bisa di @";
 
 export default async function(client: Client, user: Member | Member["user"], oldUser: JSONMember | JSONMember["user"] | null) {
   try {
-    if (!oldUser) return;
+    if (!oldUser) {
+      return;
+    };
 
     // must be at least 3 alphanumeric characters
     const regex = /[\d\w]{3,}/gim;
-    const random = randomBytes(3).toString("hex");
 
-    // guildMemberUpdate
-    if ("nick" in user && "nick" in oldUser) {
-      if (user.nick !== null && user.nick !== oldUser?.nick) {
-        if (!user.nick.match(regex)) {
-          return user.edit({
-            nick: `biar bisa ditag ${random}`
-          });
-        };
-      };
-    }
-    
-    // userUpdate
-    if (!("nick" in user) && !("nick" in oldUser)) {
-      let oldUsernameSystem = user.globalName == null && user.username !== oldUser.username;
-      let newUsernameSystem = user.globalName !== null && user.globalName !== oldUser?.globalName;
+    if (("nick" in user && "nick" in oldUser) && (user.nick !== null && user.nick !== oldUser?.nick) && !user.nick.match(regex)) {
+      const nick = await getCommonSurname() || fallbackName;
 
-      if ((oldUsernameSystem && !user.username.match(regex)) || (newUsernameSystem && !user?.globalName?.match(regex))) {
-        return await client.rest.guilds.editMember(gmdiGuildID, user.id, { 
-          nick: `biar bisa ditag ${random}`
-        });
+      await user.edit({ nick });
+
+      return;
+    };
+
+    if (("globalName" in user && "globalName" in oldUser)) {
+      let oldUsername = user.globalName == null && user.username !== oldUser.username;
+      let newUsername = user.globalName !== null && user.globalName !== oldUser?.globalName;
+
+      if ((oldUsername && !user.username.match(regex)) || (newUsername && !user?.globalName?.match(regex))) {
+        const nick = await getCommonSurname() || fallbackName;
+
+        await client.rest.guilds.editMember(gmdiGuildID, user.id, { nick });
+
+        return;
       };
     };
   } catch (error) {
-    return console.error(error);
+    console.error(error);
   };
+
+  return;
+};
+
+async function getCommonSurname() {
+  if (cachedSurnames.size <= 0) {
+    const endpoint = "https://gist.githubusercontent.com/maulvi/e443e22b82a1dc24e14344b47f0a80ea/raw/b8465b38366622daad2d841054ad155d16c5c3a5/nama.txt";
+    const request = await fetch(endpoint);
+    if (!request.ok) return null;
+    
+    const data = await request.text();
+
+    data.split(/[\n]/gim).forEach(name => cachedSurnames.add(name.trim()));
+  };
+
+  const array = Array.from(cachedSurnames);
+  const randomizedValue = array[randomNumber(0, array.length)];
+
+  return (
+    typeof randomizedValue === "string" ?
+    (randomizedValue.charAt(0).toUpperCase() + randomizedValue.slice(1)) :
+    null
+  );
 };
